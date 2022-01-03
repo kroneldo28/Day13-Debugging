f0f1533b4be4f7a012960d100b950ca3292b4e58
Ronel <kroneldo28@users.noreply.github.com>
2022-01-03 12:41:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+############DEBUGGING#####################
+
+# # Describe Problem
+# def my_function():
+#   for i in range(1, 20):
+#     if i == 20:
+#       print("You got it")
+# my_function()
+# Here the code does nothing because i never gets to equal 20
+
+# # Reproduce the Bug
+# from random import randint
+# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+# dice_num = randint(0, 5)
+# print(dice_imgs[dice_num])
+# # Randint(1,6) returns a number between 1 and 6 
+# # and every time that number is 6 we get out of range according to the list.
+
+# # Play Computer
+# year = int(input("What's your year of birth? "))
+# if year > 1980 and year <= 1994:
+#   print("You are a millenial.")
+# elif year > 1994:
+#   print("You are a Gen Z.")
+
+# # Fix the Errors
+# age = int(input("How old are you? "))
+# if age > 18:
+#   print(f"You can drive at age {age}.")
+
+# #Print is Your Friend
+# pages = 0
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page = int(input("Number of words per page: "))
+# total_words = pages * word_per_page
+# print(total_words)
+
+#Use a Debugger
+def mutate(a_list):
+  b_list = []
+  for item in a_list:
+    new_item = item * 2
+    b_list.append(new_item)
+  print(b_list)
+
+mutate([1,2,3,5,8,13])
